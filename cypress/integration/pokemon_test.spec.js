967a110f50e406b373880c6b905715b701753350
Thais Tiemi Obana <2102818@alunoimpacta.com.br>
2022-05-21 20:48:35 -0300

    changed tests based on expect assertion

--- cypress/integration/pokemon_test.spec.js
+++ cypress/integration/pokemon_test.spec.js
@@ -1,19 +1,15 @@
 describe('API testing with cypress using PokeAPI', () =>{
 
-    it('Validade response status code', () =>{
-        cy.request('pokemon/lucario')
-                .its('status')
-                .should('equal', 200)
-    })
-                
-    it('Validate response json', () =>{
-        cy.request('pokemon/lucario')
-        .its('body')
-        .should('include', 
-            { name: 'lucario',
-              order: 573,
-              weight: 540
-            })
+    it('Calling a non existent Pokemon validating an error', ()=>{
+        cy.request({url: 'pokemon/lucario', 
+                    failOnStatusCode: false}).then((resp) =>{
+                        expect(resp.status).to.eq(200)
+                        expect(resp.body).to.include( 
+                            { name: 'lucario',
+                              order: 573,
+                              weight: 540
+                            })
+                    })
     })
 
     it('Calling a non existent Pokemon validating an error', ()=>{
